a4b528cb3efddbb3e6cba521ac5c2e1568fb2487
LogosIA <melkysalem@gmail.com>
2026-05-20 10:49:20 +0200

    fix: register save_last_folder_cmd in invoke_handler

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -181,6 +181,7 @@ pub fn run() {
             config::commands::save_ai_key_cmd,
             config::commands::delete_ai_key_cmd,
             config::commands::save_search_key_cmd,
+            config::commands::save_last_folder_cmd,
             video::commands::start_video_download,
             video::commands::get_video_formats,
             video::commands::start_playlist_download,
